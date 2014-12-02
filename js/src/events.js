b204a5f0b627bb9a028e7f997b9de35cdbd8a1e3
Anders Fisher <atleastimtrying@gmail.com>
2014-12-02 15:04:17 +0000

    events written and tested

--- js/src/events.js
+++ js/src/events.js
@@ -1,5 +1,33 @@
 (function(){
   window.crampons.Events = function(){
+    var data = {};
 
+    var subscribe = function(name, callback){
+      if(!data[name]){
+        data[name] = [callback];
+      }else{
+        data[name].push(callback);
+      }
+    };
+
+    var publish = function(name, options){
+      if(data[name]){
+        data[name].forEach(function(fn){
+          fn(options);
+        });
+      }
+    };
+
+    var open = function(){
+      return data;
+    };
+
+    return {
+      subscribe: subscribe,
+      publish: publish,
+      sub: subscribe,
+      pub: publish,
+      open: open
+    };
   };
 }());
--- js/test/events_spec.js
+++ js/test/events_spec.js
@@ -4,4 +4,135 @@ describe("Events", function() {
     expect(window.crampons.Events).toBeDefined();
   });
 
+  describe("as an instance", function() {
+    beforeEach(function() {
+      this.events = new window.crampons.Events();
+    });
+
+    it("can be instantiated", function(){
+      expect(this.events).toBeDefined();
+    });
+
+    describe("#open", function(){
+      it("is a function", function(){
+        expect(typeof this.events.open).toEqual('function');
+      });
+
+      it("returns an object", function(){
+        expect(typeof this.events.open()).toEqual('object');
+      });
+    });
+
+    describe("#subscribe", function(){
+      it("is a function", function(){
+        expect(typeof this.events.subscribe).toEqual('function');
+      });
+
+      it("adds an event and callback to the data", function(){
+        var callback = function(){};
+        this.events.subscribe('foo', callback);
+        var bindings = this.events.open();
+        expect(bindings.foo).toEqual([callback]);
+      });
+
+      it("adds a multiple callbacks to an event", function(){
+        var callback = function(){};
+        var callback_two = function(){};
+        this.events.subscribe('foo', callback);
+        this.events.subscribe('foo', callback_two);
+        var bindings = this.events.open();
+        expect(bindings.foo).toEqual([callback, callback_two]);
+      });
+
+      it("should handle callbacks being triggered twice");
+    });
+
+    describe("#publish", function(){
+      it("is a function", function(){
+        expect(typeof this.events.publish).toEqual('function');
+      });
+
+      it("should call a bound callback", function(){
+        var test_object = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object, 'foo');
+        this.events.subscribe('foo', foo_spy);
+        this.events.publish('foo');
+        expect( foo_spy ).toHaveBeenCalled();
+      });
+
+      it("should pass an options object to a callback", function(){
+        var test_object = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object, 'foo');
+        var options = { bar: 'baz' };
+        this.events.subscribe('foo', foo_spy);
+        this.events.publish('foo', options);
+        expect( foo_spy ).toHaveBeenCalledWith(options);
+      });
+
+      it("should call two bound callbacks", function(){
+        var test_object_1 = {
+          foo: function(){}
+        };
+        var test_object_2 = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object_1, 'foo');
+        var foo2_spy = spyOn(test_object_2, 'foo');
+        this.events.subscribe('foo', foo_spy);
+        this.events.subscribe('foo', foo2_spy);
+        this.events.publish('foo');
+        expect( foo_spy ).toHaveBeenCalled();
+        expect( foo2_spy ).toHaveBeenCalled();
+      });
+      it("should pass an options block to two bound callbacks", function(){
+        var test_object_1 = {
+          foo: function(){}
+        };
+        var test_object_2 = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object_1, 'foo');
+        var foo2_spy = spyOn(test_object_2, 'foo');
+        var options = { bar: 'baz' };
+        this.events.subscribe('foo', foo_spy);
+        this.events.subscribe('foo', foo2_spy);
+        this.events.publish('foo', options);
+        expect( foo_spy ).toHaveBeenCalledWith(options);
+        expect( foo2_spy ).toHaveBeenCalledWith(options);
+      });
+
+      it("should not call an unbound callback", function(){
+        var test_object = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object, 'foo');
+        this.events.subscribe('foo', foo_spy);
+        this.events.publish('baz');
+        expect( foo_spy ).not.toHaveBeenCalled();
+      });
+
+      it("should call a bound callback", function(){
+        var test_object = {
+          foo: function(){}
+        };
+        var foo_spy = spyOn(test_object, 'foo');
+        this.events.subscribe('foo', foo_spy);
+        this.events.publish('foo');
+        expect( foo_spy ).toHaveBeenCalled();
+      });
+    });
+
+    describe("aliases", function(){
+      it("has pub equal publish", function(){
+        expect(this.events.pub).toEqual(this.events.publish);
+      });
+      it("has sub equal subscribe", function(){
+        expect(this.events.sub).toEqual(this.events.subscribe);
+      });
+    });
+  });
 });
