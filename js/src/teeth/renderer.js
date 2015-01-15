a15a82125f969c562cd04fa6e4262eb04de3a830
Anders Fisher <atleastimtrying@gmail.com>
2015-01-15 08:09:39 +0000

    renderer is defined

---
+++ js/src/teeth/renderer.js
@@ -0,0 +1,6 @@
+(function(){
+
+  window.crampons.teeth.Renderer = function(){
+  };
+
+})();
---
+++ js/test/teeth/renderer_spec.js
@@ -0,0 +1,5 @@
+describe("renderer", function(){
+  it("is defined", function(){
+    expect(window.crampons.teeth.Renderer).toBeDefined();
+  });
+});
