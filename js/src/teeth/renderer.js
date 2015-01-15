cf13a35368c4639f469bb3a0110218ff371050b5
Anders Fisher <atleastimtrying@gmail.com>
2015-01-15 08:26:54 +0000

    append and write now events in renderer

--- js/src/teeth/renderer.js
+++ js/src/teeth/renderer.js
@@ -1,6 +1,13 @@
 (function(){
 
-  window.crampons.teeth.Renderer = function(){
+  window.crampons.teeth.Renderer = function(events, dom_element){
+    events.subscribe('renderer:write',function(options){
+      dom_element.innerHTML = options.data.content;
+    });
+
+    events.subscribe('renderer:append',function(options){
+      dom_element.innerHTML += options.data.content;
+    });
   };
 
 })();
--- js/test/teeth/renderer_spec.js
+++ js/test/teeth/renderer_spec.js
@@ -2,4 +2,34 @@ describe("renderer", function(){
   it("is defined", function(){
     expect(window.crampons.teeth.Renderer).toBeDefined();
   });
+
+  var events, dom_element, renderer;
+  beforeEach(function(){
+    events = new crampons.Events();
+    dom_element = document.createElement('span');
+    renderer = new crampons.teeth.Renderer(events, dom_element);
+  });
+
+  it("should write to the dom element", function(){
+
+    events.publish('renderer:write', {
+      data:{
+        content: 'foo'
+      }
+    });
+
+    expect(dom_element.innerHTML).toEqual('foo');
+  });
+
+  it("should append content to the dom element", function(){
+    dom_element.innerHTML = 'foo';
+
+    events.publish('renderer:append',{
+      data:{
+        content: 'bar'
+      }
+    });
+
+    expect(dom_element.innerHTML).toEqual('foobar');
+  });
 });
