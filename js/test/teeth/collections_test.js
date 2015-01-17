b0727b8d39d85210fb08c08084d7f7ad6ae49979
Anders Fisher <atleastimtrying@gmail.com>
2015-01-17 20:22:44 +0000

    collections idea jotted down

---
+++ js/test/teeth/collections_test.js
@@ -0,0 +1,22 @@
+describe('Collections', function(){
+
+  it("should be defined");
+
+  it("should be able to create a new named collection");
+
+  it("should be able to add an item to the collection");
+
+  it("should be able to add multiple items to the collection");
+
+  it("should be able to find and return an item in a collection");
+
+  it("should be able to find a subset of a collection");
+
+  it("should be able to update an item in a collection");
+
+  it("should be able to merge two similar collections");
+
+  it("should be able to find an item by a predefined attribute");
+
+  it("should be able to validate a collection by predefined attribute");
+});
